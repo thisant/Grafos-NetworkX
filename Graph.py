f356714e4f2ed2e866cc2260fa6fe0e02a25270f
Thiago Santana <39104645+thisant@users.noreply.github.com>
2021-03-30 14:32:14 -0300

    Update com métodos básicos
    Foram adicionados os métodos ehConexo, ehCompleto, ehConexo

--- Graph.py
+++ Graph.py
@@ -3,15 +3,51 @@ import matplotlib.pyplot as plt
 
 G = nx.Graph()
 
-G.add_node('A'), ('B'), ('C'), ('D'), ('E')
+G.add_node(0), (1), (2), (3), (4)
 
-G.add_edge('A','B')
-G.add_edge('B','C')
-G.add_edge('C','D')
-G.add_edge('D','E')
-G.add_edge('E','A')
-G.add_edge('B','E')
+no = G.number_of_nodes()
 
-plt.figure(5)
+G.add_edge(0,1)
+G.add_edge(1,2)
+G.add_edge(2,3)
+G.add_edge(3,4)
+G.add_edge(4,0)
+G.add_edge(1,4)
+
+arresta = G.number_of_edges()
+
+plt.figure(1)
 nx.draw_networkx(G, pos=nx.spring_layout(G), with_labels=True)
 plt.show()
+
+def ehCompleto(self):
+  if (arresta == no*(no-1)/2):
+    return True
+  else:
+    return False
+
+def buscaProfundidade(G, v, visitado):
+    visitado[v] = True
+    for u in arresta[v]:
+        if not visitado[u]:
+            buscaProfundidade(G, u, visitado)
+
+def ehConexo(self):
+  for i in range(no):
+    visitado = [False] * no
+    buscaProfundidade(G, i, visitado)
+    for b in visitado:
+      if not b:
+        return False
+  return True
+
+def ehRegular(self):  
+  lista= list(G.degree([0, 1, 2, 3, 4]))
+  grau = [indice [1] for indice in lista]
+  resultado = False;
+  if len(grau) > 0 :
+    resultado = all(item == grau[0] for item in list1)
+  if resultado:
+    return True
+  else:        
+    return False
